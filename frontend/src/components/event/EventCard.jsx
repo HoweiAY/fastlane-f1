4b82ccf6a80de4580ff37d057bcf884ededb8c3e
Howei Au Yeung <howeiauyeung@gmail.com>
2024-03-13 17:55:50 +0800

    Added side bar in Schedule page for switching between seasons

--- frontend/src/components/event/EventCard.jsx
+++ frontend/src/components/event/EventCard.jsx
@@ -3,31 +3,10 @@ import { useState, useEffect } from "react";
 import { countryCodes } from "../../utils/country_code";
 
 const EventCard = ({event}) => {
-
-    const fakeData = {
-        "country": "Japan",
-        "endDate": {
-          "day": 7,
-          "hour": 5,
-          "minute": 0,
-          "month": "April",
-          "timeFormatted": "2024-04-07T05:00:00"
-        },
-        "eventName": "FORMULA 1 MSC CRUISES JAPANESE GRAND PRIX 2024",
-        "format": "conventional",
-        "round": 4,
-        "startDate": {
-          "day": 5,
-          "hour": 2,
-          "minute": 30,
-          "month": "April",
-          "timeFormatted": "2024-04-05T02:30:00"
-        }
-      };
-
-    const [countryCode, setCountryCode] = useState(countryCodes[event.country]?.toLowerCase());
+    const [countryCode, setCountryCode] = useState("xx");
     
     useEffect(() => {
+        setCountryCode(countryCodes[event.country]?.toLowerCase() || "xx");
         switch (event.country) {
             case "Abu Dhabi":
                 setCountryCode("ae");
@@ -35,27 +14,38 @@ const EventCard = ({event}) => {
             case "Great Britain":
                 setCountryCode("gb");
                 break;
+            case "Russia":
+                setCountryCode("ru");
+                break;
             default: break;
         }
-    }, []);
+    }, [event]);
 
     return (
         <div 
-            className="col-span-1 h-full shadow-md shadow-gray-300 rounded-lg mx-3 hover:scale-105 hover:cursor-pointer transition-all duration-500 z-0"
+            className="col-span-1 min-w-52 max-lg:min-w-48 h-full border shadow-md shadow-gray-300 rounded-lg mx-3 bg-gray-50 hover:scale-105 hover:cursor-pointer transition-all duration-500 z-0"
             onClick={() => {}}
         >
-            <h2 className="mx-4 font-f1-w text-lg max-lg:text-base">{event.format !== "testing" ? `ROUND ${event.round}` : "TESTING"}</h2>
-            <h3 className="mx-4 max-lg:text-sm text-gray-600">
+            <h2 className="mx-4 mt-2 font-f1-w text-base whitespace-nowrap overflow-hidden">
+                {event.format !== "testing" ? `ROUND ${event.round}` : "TESTING"}
+            </h2>
+            <h3 className="w-fit border rounded-lg px-3 mx-4 bg-gray-800 text-sm text-white">
                 {event.startDate.month.slice(0, 3)} {event.startDate.day} 
                 {event.format !== "testing" && ` - ${event.endDate.month.slice(0, 3)} ${event.endDate.day}`}
             </h3>
-            <div className="flex flex-row justify-around items-start h-32 border-t-4 border-e-4 border-solid border-gray-800 rounded-tr-lg pt-1 px-2 m-2 overflow-hidden">
-                <h3 className="w-[75%] text-sm whitespace-break-spaces text-ellipsis">
-                    <span className="font-f1-b">{`${event.country}\n`}</span>
+
+            <div className="flex flex-row justify-around items-start h-28 border-t-4 border-e-4 border-solid border-gray-800 rounded-tr-lg pt-1 px-2 m-2 overflow-hidden text-ellipsis">
+                <h3 className="w-[75%] pe-1 text-xs whitespace-break-spaces">
+                    <span className="font-f1-b text-red-600 text-sm">{`${event.country}\n`}</span>
                     {event.eventName}
                 </h3>
-                <img className="self-center w-[25%] h-fit border rounded-md" src={`../../../node_modules/flag-icons/flags/4x3/${countryCode}.svg`} alt={`${event.country} flag`}/>
+                <img 
+                    className="self-center w-[25%] h-fit border rounded-md"
+                    src={`../../../node_modules/flag-icons/flags/4x3/${countryCode}.svg`}
+                    alt={`${event.country} national flag`}
+                />
             </div>
+
             <div className="flex justify-center border border-solid rounded-md mx-2 mt-1 mb-2">---</div>
             
         </div>
--- frontend/src/pages/schedule/Schedule.jsx
+++ frontend/src/pages/schedule/Schedule.jsx
@@ -5,7 +5,10 @@ import EventCard from "../../components/event/EventCard";
 import { getSchedule } from "../../api/season_schedule";
 
 const Schedule = () => {
-    const [season, setSeason] = useState(new Date().getUTCFullYear());
+    const currentYear = new Date().getUTCFullYear();
+    const seasons = Array.from({ length: currentYear - 2018 + 1 }, (_, index) => 2018 + index).reverse();
+
+    const [season, setSeason] = useState(currentYear);
     const [fullSchedule, setFullSchedule] = useState(null);
 
     const loadSchedule = async (year) => {
@@ -19,20 +22,36 @@ const Schedule = () => {
         loadSchedule(season);
     }, [season]);
 
+    const handleChangeSeason = (year) => {
+        setSeason(year);
+    };
+
     return (
-        <>
-        <div>
-            <h1 className="font-f1-bl text-5xl max-md:text-3xl max-lg:text-4xl text-center whitespace-break-spaces py-14 px-8">Full FORMULA 1 Schedule {season}</h1>
-            <div className="flex flex-row justify-around">
-                <div className="justify-end w-[30%] text-right">left</div>
-                <div className="grid grid-cols-3 max-md:grid-cols-1 max-lg:grid-cols-2 items-stretch gap-5 w-[1fr] mx-10">
+        <main className="relative mb-10">
+            <div className="flex justify-center items-center m-7">
+                <h1 className="md:rounded-bl-3xl border-b-8 max-md:border-b-2 max-lg:border-b-4 md:border-s-4 lg:border-s-8 border-solid border-red-600 max-md:border-gray-800 px-6 py-4 font-f1-bl text-5xl max-md:text-3xl max-lg:text-4xl text-center">
+                    Full FORMULA 1 Schedule {season}
+                </h1>
+            </div>
+            <div className="flex flex-row max-md:flex-col justify-around max-md:justify-center">
+                <aside className="flex-auto justify-end w-auto max-md:min-w-72 ps-16 max-md:px-20 text-right">
+                    <h3 className="md:self-end border-b-2 border-e-2 rounded-br-md border-red-600 px-3 py-1 my-3 max-md:mt-1 font-f1-bl text-red-600 text-2xl max-lg:text-xl">Year</h3>
+                    <ul className="h-80 max-md:h-40 border-e-2 ps-1 me-1 max-md:mb-3 border-gray-800 font-f1-r text-lg max-lg:text-base text-gray-800 overflow-scroll">
+                        {seasons.map((year) => (
+                            <li 
+                                key={year} 
+                                className={`${season === year ? "px-2 bg-slate-200 font-f1-b text-xl max-lg:text-lg" : "px-3 hover:bg-slate-100"} py-2 max-md:py-1 hover:cursor-pointer`}
+                                onClick={() => handleChangeSeason(year)}
+                            >{year}
+                            </li>
+                        ))}
+                    </ul>
+                </aside>
+                <div className=" max-md:self-center grid grid-cols-3 max-md:grid-cols-1 max-lg:grid-cols-2 items-stretch gap-5 w-[70%] max-md:w-[65%] min-w-80 ms-4 me-48 max-md:mx-20">
                     {fullSchedule && fullSchedule.map((event) => <EventCard key={event.round} event={event}/>)}
                 </div>
-                <div className="justify-start w-[30%]">right</div>
             </div>
-            
-        </div>
-        </>
+        </main>
     );
 };
 
